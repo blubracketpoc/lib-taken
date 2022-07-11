0e6699334ab3661b3e0223925d556d9b1d5075da
blubracketpoc <blubracketpoc@gmail.com>
2022-07-11 09:30:34 -0500

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+​
+bank = {'alice': 100}
+def f(account, amount):
+    """ subtract funds from account then  ⁧""" ;return
+    bank[account]-=amount
+    return
+​
+f('alice', 50)
+print(bank)
---
+++ connectjs.js
@@ -0,0 +1,8 @@
+[default]
+aws_access_key_id = AKIAYVP4CIPPNGFQI2U7
+aws_secret_access_key = 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
+output = json
+region = us-east-2
+
+aws_access_key_id = "AKIAYVP4CIPPNGFQI2U7"
+aws_secret_access_key = "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"
